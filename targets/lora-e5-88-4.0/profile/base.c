81b948a4432bb006b108f35fa74242f6fda6a714
Thomas Ball <thomas.j.ball@outlook.com>
2025-07-26 14:45:21 -0700

    remove devicescript

--- targets/lora-e5-88-4.0/profile/base.c
+++ targets/lora-e5-88-4.0/profile/base.c
@@ -2,24 +2,27 @@
 #include "tinyhw.h"
 
 #include "jd_client.h"
-#include "devicescript/devicescript.h"
+// no more devicescript
+// #include "devicescript/devicescript.h"
 
 FIRMWARE_IDENTIFIER(0x30085c3f, "JM-Lora E5 88 v4.0");
 
 #define PROG_SIZE JD_FLASH_SETTINGS_SIZE
 #define PROG_START (0x08000000 + (JD_FLASH_SIZE - BL_SIZE - PROG_SIZE))
 
+/*
+
 static const devicescriptmgr_cfg_t devs_cfg = {
     .program_base = (void *)PROG_START,
     .max_program_size = PROG_SIZE,
 };
+*/
 
 void app_init_services() {
     extcloud_init();
 
     jd_role_manager_init();
-    devicescriptmgr_init(&devs_cfg);
-
-    devscloud_init(&extcloud);
-    tsagg_init(&extcloud);
+    // devicescriptmgr_init(&devs_cfg);
+    // devscloud_init(&extcloud);
+    // tsagg_init(&extcloud);
 }
--- targets/star-brain/profile/devicescript.c
+++ targets/star-brain/profile/devicescript.c
@@ -2,24 +2,26 @@
 #include "tinyhw.h"
 
 #include "jd_client.h"
-#include "devicescript/devicescript.h"
+// #include "devicescript/devicescript.h"
 
 FIRMWARE_IDENTIFIER(0x3d1fe52f, "JM-DeviceScript Star-Brain v3.3");
 
 #define PROG_SIZE JD_FLASH_SETTINGS_SIZE
 #define PROG_START (0x08000000 + (JD_FLASH_SIZE - BL_SIZE - PROG_SIZE))
 
+/*
 static const devicescriptmgr_cfg_t devs_cfg = {
     .program_base = (void *)PROG_START,
     .max_program_size = PROG_SIZE,
 };
+*/
 
 void app_init_services() {
-    extcloud_init();
+    //extcloud_init();
 
     jd_role_manager_init();
-    devicescriptmgr_init(&devs_cfg);
+    //devicescriptmgr_init(&devs_cfg);
 
-    devscloud_init(&extcloud);
-    tsagg_init(&extcloud);
+    //devscloud_init(&extcloud);
+    // tsagg_init(&extcloud);
 }
